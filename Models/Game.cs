5aec439d10f80d1fc9d1cc694e757c93d3872d3b
Christopher <drakochris1992@gmail.com>
2021-04-07 09:15:42 -0400

    model start

---
+++ Models/Game.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+namespace razor_pg_ef.Models{
+
+    public class Game{
+
+        public int GameId {get; set;}
+        public string Title {get; set;}
+
+        [DataType (DataType.Date)]
+        public DateTime ReleaseDate {get; set;}
+        public decimal Price {get; set;}
+    }
+}
